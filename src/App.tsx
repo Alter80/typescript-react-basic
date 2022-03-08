8fe7aa28798a62417b68c6f555c343a41e25fd5a
Alter80 <fahimatul8@Gmail.com>
2022-03-08 13:53:45 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddToList from './components/AddToList';
+import List from './components/List';
+import { IsState } from './components/interface/IsState';
+
+// interface IsState {
+//   people: {
+//     name: string,
+//     age: number,
+//     url: string,
+//     note?: string
+//   }[]
+// }
+
 
 function App() {
+  const [people, setPeople] = useState<IsState['people']>([
+    {
+      name: 'Fahim Hossain',
+      url: 'https://avatars.githubusercontent.com/u/77225912?v=4',
+      age: 25,
+      note: "Allergic to dust"
+    }
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>People Invited to my party</h1>
+      <List people={people}></List>
+      <AddToList people={people} setPeople={setPeople}></AddToList>
+    </div >
   );
 }
 
